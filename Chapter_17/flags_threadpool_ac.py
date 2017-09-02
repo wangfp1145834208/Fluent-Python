1244d4c6389fe12f669fc040c0e669d714e3c2da
wangfp <wangfp_nk@163.com>
2017-09-02 18:23:50 +0800

    concurrent.futures

---
+++ Chapter_17/flags_threadpool_ac.py
@@ -0,0 +1,38 @@
+from time import sleep
+
+from concurrent import futures
+
+from flags import save_flag, get_flag, show, main
+
+MAX_WORKERS = 20
+
+def download_one(cc):
+    image = get_flag(cc)
+    show(cc)
+    save_flag(image, cc.lower() + '.gif')
+    return cc
+
+def download_many(cc_list):
+    cc_list = cc_list[:5]
+    with futures.ThreadPoolExecutor(max_workers=3) as executor:
+        to_do = []
+        for cc in sorted(cc_list):
+            future = executor.submit(download_one, cc)
+            to_do.append(future)
+            msg = 'Scheduled for {}: {}'
+            print(msg.format(cc, future))
+
+        #sleep(5)
+        #print('***start operate future...')
+
+        results = []
+        for future in futures.as_completed(to_do):
+            res = future.result()
+            msg = '{} result: {!r}'
+            print(msg.format(future, res))
+            results.append(res)
+
+    return len(results)
+
+if __name__ == '__main__':
+    main(download_many)
