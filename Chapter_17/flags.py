8280e3074eae34c68b216e9493f101d53956d344
wangfp <wangfp_nk@163.com>
2017-09-05 00:23:52 +0800

    DAY 6

--- Chapter_17/flags.py
+++ Chapter_17/flags.py
@@ -7,7 +7,7 @@ import requests
 POP20_CC = ('CN IN US ID BR PK NG BD RU JP '
             'MX PH VN ET EG DE IR TR CD FR').split()
 
-BASE_URL = 'http://flupy.org/data/flags'
+BASE_URL = 'http://127.0.0.1:8001/flags'
 
 DEST_DIR = 'downloads/'
 
